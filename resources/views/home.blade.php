eb33daa9406baa4677363d04f985028e827083a6
unknown <ratipriya.inforoot@gmail.com>
2023-04-14 16:46:24 +0530

    reskk

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Fitness Way Employee Portall</title>
+  <title>Fitness Way Employee PortaL</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
   <!-- Google Fonts -->
